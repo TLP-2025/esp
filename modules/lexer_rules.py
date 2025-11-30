fc2a99ddd8c90f1e2d6996208bcda3668104b785
JDAlvarenga <37277109+JDAlvarenga@users.noreply.github.com>
2025-11-30 00:04:54 -0600

    remove prefix from token value

--- modules/lexer_rules.py
+++ modules/lexer_rules.py
@@ -20,15 +20,41 @@ class Token(str, Enum):
 tokens = tuple(map(lambda name: Token[name].value, Token._member_names_,))
 
 
-# Regular expression rules
-## Single-character tokens.
-t_DETERMINANTE = r'det:\w+'
-t_SUSTANTIVO = r'sus:\w+'
-t_ADJETIVO = r'adj:\w+'
-t_NOMBRE_PROPIO = r'nom:\w+'
-t_PREPOSICION = r'pre:\w+'
-t_VERBO = r'ver:\w+'
-t_ADVERBIO = r'adv:\w+'
+def t_DETERMINANTE(t):
+    r'det:\w+'
+    t.value = t.value[t.value.index(':')+1:]
+    return t
+    
+def t_SUSTANTIVO(t):
+    r'sus:\w+'
+    t.value = t.value[t.value.index(':')+1:]
+    return t
+    
+def t_ADJETIVO(t):
+    r'adj:\w+'
+    t.value = t.value[t.value.index(':')+1:]
+    return t
+    
+def t_NOMBRE_PROPIO(t):
+    r'nom:\w+'
+    t.value = t.value[t.value.index(':')+1:]
+    return t
+    
+def t_PREPOSICION(t):
+    r'pre:\w+'
+    t.value = t.value[t.value.index(':')+1:]
+    return t
+    
+def t_VERBO(t):
+    r'ver:\w+'
+    t.value = t.value[t.value.index(':')+1:]
+    return t
+    
+def t_ADVERBIO(t):
+    r'adv:\w+'
+    t.value = t.value[t.value.index(':')+1:]
+    return t
+    
 
 t_NO = r'no'
 t_Y = r'y'
